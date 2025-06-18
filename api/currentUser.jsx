4e8528daf74a43560c63d87a9ac0e7914383e2ad
tarekbinjaman <tarekbinjaman@gmail.com>
2025-06-19 03:27:09 +0600

    current user done

--- api/currentUser.jsx
+++ api/currentUser.jsx
@@ -1,8 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../axiosSecure/axiosSecure";
 import UseAuth from "../Context/UseAuth";
 
-const currentUser = () => {
+const currentUser = (email) => {
     const axiousSecure = useAxiosSecure();
     const {user} = UseAuth();
-    const query = 
+    const query = useQuery({
+        queryKey: ['userList', email],
+        queryFn: async () => {
+            const res = await axiousSecure.get(`/users?email=${email}`);
+            return res.data;
+        },
+        enabled: !!email,
+        throwOnError: (err) => console.error('User list fetch error', err)
+    });
+    return [query.data ?? [], query.refetch, user?.email]
 };
+
+export default currentUser;
