a30468d0a12bdd28301b04f1130d7e3cd40e8a11
tarekbinjaman <tarekbinjaman@gmail.com>
2025-06-22 16:44:14 +0600

    working on add product feature

--- AdminDashboard/routes/AddProduct.jsx
+++ AdminDashboard/routes/AddProduct.jsx
@@ -1,14 +1,15 @@
 import axios from 'axios';
+import { div, img } from 'framer-motion/client';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
-    const {register, handlesubmit, formstate: {errors}} = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const [imageUrls, setImageUrls] = useState([]);
     const [uploading, setUploading] = useState(false);
     const [errorMsg, setErrorMsg] = useState('');
 
-    const imgbbApiKey = 'a6dfbfb78f386d6ccb1ebdc2a79d3e58'; // imgbb api key
+    const imgbbApiKey = import.meta.env.VITE_imgbbApiKey; // imgbb api key
 
     const handleImageUpload = async (files) => {
         setUploading(true);
@@ -47,6 +48,58 @@ const AddProduct = () => {
     return (
         <div className='border border-black'>
             <h1>Add Poduct </h1>
+            <form onSubmit={handleSubmit(onsubmit)} className='max-w-4xl mx-auto p-6 bg-white rounded-xl shadow space-y-6'>
+            <div className='form-control'>
+            <input 
+            {...register('title', {required: 'Title required'})}
+            placeholder="Product Title"
+            className='w-full border p-2 rounded'
+            />
+            {errors.title && <p className='text-red-500'>{errors.title.message}</p>}
+            </div>
+            <div className='form-control'>
+                <textarea
+                {...register('description', {required: 'Product description required'})}
+                placeholder='Prouduct description'
+                className='w-full border p-2 rounded'
+                />
+            {errors.description && <p className='text-red-500'>{errors.description.message}</p>}
+            </div>
+            <div className='form-control'>
+                <input 
+                {...register('price', {required: 'Price is required'})}
+                placeholder='Price'
+                className='w-full border p-2 rounded'
+                />
+                {errors.price && <p className='text-red-500'>{errors.price.message}</p>}
+            </div>
+            <div className='form-control'>
+            <label className='block font-semibold'>Upload Product Images (Max: 4)</label>
+            <input type="file"
+                accept="image/*"
+                multiple
+                onChange={(e) => handleImageUpload(e.target.files)}
+            />
+            {uploading && <p className='text-blue-500'>Uploading...</p>}
+            {errorMsg && <p className='text-red-500'>{errorMsg}</p>}
+            {
+                imageUrls.length > 0 && (
+                    <div className='mt-2 grid grid-cols-2 gap-2'>
+                        {(imageUrls.map((url, i) => (
+                            <img key={i} src={url} alt={`preview-${i}`} className='w-full h-32 object-cover rounded' />
+                        )))}
+                    </div>
+                )
+            }
+            </div>
+            <button
+            type='submit'
+            className='bg-black text-white px-6 py-2 rounded hover:bg-gray-800 disabled:opacity-50'
+            disabled={uploading}
+            >
+                Submit Product
+            </button>
+            </form>
         </div>
     );
 };
