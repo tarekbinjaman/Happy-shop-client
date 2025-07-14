096dac71cbeb718c269c14cc59f4d547ca6e4adb
tarekbinjaman <tarekbinjaman@gmail.com>
2025-07-14 12:04:55 +0600

    done

--- Shared/Footer.jsx
+++ Shared/Footer.jsx
@@ -9,16 +9,16 @@ const Footer = () => {
     return (
         <div className='bg-[#f0f0f0]'>
             <div className='flex justify-center'>
-            <div className='flex justify-center max-w-7xl py-8'>
+            <div className='flex md:flex-row flex-col space-y-2 md:space-y-0 justify-center max-w-7xl py-8 gap-10'>
 
            
             <div className='flex flex-col space-y-3'>
-                <h1 className='text-4xl' style={{ fontFamily: 'Integral CF' }}>SHOP.CO</h1>
-                <p className='text-gray-400 w-1/2'>We have clothes that suits your style and
+                <h1 className='text-4xl lg:ml-0 ml-4' style={{ fontFamily: 'Integral CF' }}>SHOP.CO</h1>
+                <p className='text-gray-400 lg:w-1/2 lg:ml-0 ml-4'>We have clothes that suits your style and
                     which you•re proud to wear. From
                     women to men.</p>
                     {/* component 1 */}
-                <div className='flex gap-2 items-center'>
+                <div className='flex gap-2 items-center lg:ml-0 ml-4'>
                     {/* icons */}
                     <div className='p-1 border rounded-full bg-white'>
                     <FaTwitter className='text-xl' />
@@ -34,9 +34,9 @@ const Footer = () => {
                     </div>
                 </div>
                 </div>
-                <div className='flex gap-8'>       
+                 <div className='flex md:flex-row flex-col gap-14 md:gap-x-24 md:gap-y-0'> 
                 {/* component 2 */}
-                <div className='flex gap-14'>
+                <div className='flex md:gap-6 gap-20 lg:ml-0 ml-4'>
                 <div>
                     <h1 className='mb-4'>COMPANY</h1>
                     <ul className='textarea-md text-gray-500 space-y-2'>
@@ -57,7 +57,7 @@ const Footer = () => {
                 </div>
                 </div>
                 {/* component 3 */}
-                <div className='flex gap-14'> 
+                <div className='flex md:gap-6 gap-20 lg:ml-0 ml-4'> 
                 <div>
                     <h1 className='mb-4'>FAQ</h1>
                     <ul className='textarea-md text-gray-500 space-y-2'>
@@ -80,13 +80,13 @@ const Footer = () => {
                 </div>
              </div>
              </div>
-             <div className='max-w-7xl  mx-auto'>
-             <div className='divider w-full'></div>
+             <div className='max-w-7xl  mx-auto '>
+             <div className='divider w-full '></div>
              </div>
-             <div className='max-w-7xl mx-auto'>
-            <div className='flex justify-between '>
-                <p className='text-gray-400'>Shop.co © 2000-2023. All Rights Reserved</p>
-                <div className='flex items-center gap-3'>
+             <div className='lg:max-w-7xl w-full mx-auto mb-8'>
+            <div className='flex lg:flex-row flex-col lg:justify-between justify-center'>
+                <p className='text-gray-400 md:text-start text-center'>Shop.co © 2000-2023. All Rights Reserved</p>
+                <div className='flex items-center md:justify-start justify-center gap-3 md:mt-0 mt-2'>
                     <SiVisa className='text-blue-500 text-4xl' />
                     <RiMastercardFill className='text-4xl' />
                     <FaCcPaypal  className='text-3xl' />
