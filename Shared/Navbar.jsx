4ffe6f989ec4895c19bee9a78e62b03c5e48c7e6
tarekbinjaman <tarekbinjaman@gmail.com>
2025-07-30 02:14:23 +0600

    search bar added

--- Shared/Navbar.jsx
+++ Shared/Navbar.jsx
@@ -12,6 +12,8 @@ import { TbLogout2, TbShoppingCartCheck } from 'react-icons/tb';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
 import UseAuth from '../Context/UseAuth'
 import currentUser from '../api/currentUser';
+import { SlMagnifier } from 'react-icons/sl';
+import { FaMagnifyingGlass } from 'react-icons/fa6';
 
 const Navbar = () => {
     const { logOut, user } = UseAuth();
@@ -23,6 +25,7 @@ const Navbar = () => {
     const email = user?.email;
     const [currentUerData, refetchUserList] = currentUser(email);
     const userData = currentUerData?.[0];
+    const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
     const isActive = ({ isActive }) => isActive ? 'text-blue-500 bg-slate-200' : '';
     const handleLogout = () => {
         logOut();
@@ -103,9 +106,12 @@ const Navbar = () => {
 
                 {/* cart and profile icons */}
                 <div>
-                    <div className='flex md:gap-4 gap-2 items-center whitespace-nowrap'>
+                    <div className='flex md:gap-4 gap-4 items-center whitespace-nowrap'>
                         {/* profile and cart */}
-                        <BsCart2 className='text-3xl text-gray-500 cursor-pointer hover:text-black ' />
+                        <div className='flex items-center gap-4'>
+                            <FaMagnifyingGlass className='text-2xl font-bold text-gray-500 cursor-pointer hover:text-black ' onClick={() => setIsSearchBarOpen(!isSearchBarOpen)} />
+                            <BsCart2 className='text-3xl text-gray-500 cursor-pointer hover:text-black ' />
+                        </div>
                         {user?.email
                             ? <div onClick={() => setDropdownOpen(!dropdownOpen)} ref={dropdownref}>
                                 <img
@@ -148,12 +154,26 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
-            <div className='relative w-[95%] mx-auto mt-2 md:hidden'>
-                {/* here will be search bar for small device */}
-                <input type="text" className='border w-full px-2 py-1 rounded focus:border-gray-400' id="" />
-                <button className="absolute right-[1px] top-1/2 transform -translate-y-1/2 text-white bg-black/80 px-2 py-1">
-                    Search
-                </button>
+            <div className='relative'>
+                <div class={`w-full max-w-sm min-w-[200px]  z-50 ml-5 ${isSearchBarOpen ? 'block opacity-100' : 'hidden opacity-0' } mt-4`}>
+                    <div class="relative flex items-center ">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="absolute w-5 h-5 top-2.5 left-2.5 text-slate-600">
+                            <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
+                        </svg>
+
+                        <input
+                            class="w-full bg-white backdrop-blur-md placeholder:text-slate-600 text-slate-700 text-sm border border-slate-300 rounded-md pl-10 pr-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-white focus:shadow  hover:shadow-2xl hover:shadow-black"
+                            placeholder="Search here..."
+                        />
+
+                        <button
+                            class="rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+                            type="button"
+                        >
+                            Search
+                        </button>
+                    </div>
+                </div>
             </div>
 
 
