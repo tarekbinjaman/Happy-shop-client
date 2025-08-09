0f5046d9ec3c1e543ae7d2d47298bee4ea941c78
tarekbinjaman <tarekbinjaman@gmail.com>
2025-08-10 03:23:04 +0600

    working on single page product

--- Pages/ViewProduct/ViewProduct.jsx
+++ Pages/ViewProduct/ViewProduct.jsx
@@ -9,6 +9,8 @@ const ViewProduct = () => {
     const singleProduct = products.find(items => items?._id === productId);
     const imageContainer = singleProduct?.images;
     const firstImage = imageContainer[0]?.url;
+    const filledStars = Math.floor(singleProduct?.rating);
+    const hasHalfStar = singleProduct?.rating >= 0.5;
     useEffect(() => {
         setShowImage(firstImage)
     }, [productId])
@@ -25,9 +27,9 @@ const ViewProduct = () => {
                                 {/* all images */}
                                 {
                                     imageContainer.map((img, index) => (
-                                        <img src={img?.url} alt="" 
-                                        onClick={() => {setShowImage(img?.url)}}
-                                        className={`w-full h-[133px] object-cover rounded-md
+                                        <img src={img?.url} alt=""
+                                            onClick={() => { setShowImage(img?.url) }}
+                                            className={`w-full h-[133px] object-cover rounded-md
                                         ${showImage === img?.url ? 'border-2 border-blue-500 rounded-md' : ''}
                                             `}
                                         />
@@ -38,15 +40,32 @@ const ViewProduct = () => {
                         )
                     }
                     <div className='h-[422px]'>
-                    <img src={showImage} alt=""
-                    className='w-[400px] h-full object-cover rounded-md'
-                    />
+                        <img src={showImage} alt=""
+                            className='w-[400px] h-full object-cover rounded-md'
+                        />
                     </div>
                 </div>
                 {/* text area */}
                 <div>
                     <div className='flex flex-col space-y-2'>
                         <h1 className='text-4xl font-bold uppercase'>{singleProduct?.title}</h1>
+                        {/* rating */}
+                        <div className="flex items-center space-x-1 mt-1">
+                            {[...Array(filledStars)].map((_, i) => (
+                                <span key={i} className="text-yellow-400 text-2xl">★</span>
+                            ))}
+                            {hasHalfStar && <span className="text-yellow-400">☆</span>}
+                            {[...Array(5 - filledStars - (hasHalfStar ? 1 : 0))].map((_, i) => (
+                                <span key={i} className="text-gray-300 text-2xl">★</span>
+                            ))}
+                            <span className="ml-2 text-sm text-gray-600">({singleProduct?.rating})</span>
+                        </div>
+                        {/* price area */}
+                        <div className='flex gap-4 items-center'>
+                            <span className='text-3xl font-bold'>${singleProduct?.finalPrice}</span>
+                            <span className='line-through text-gray-400 text-3xl font-semibold '>{singleProduct.price}</span>
+                            <span className='text-md bg-red-200 px-3 py-1 rounded-2xl font-semibold text-red-500'>-{singleProduct?.discount}%</span>
+                        </div>
                     </div>
                 </div>
             </div>
