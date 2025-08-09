be7525ed3ef6ebb511591450c621dd8e60a5192f
tarekbinjaman <tarekbinjaman@gmail.com>
2025-08-10 01:27:15 +0600

    in view product page image seetup done

--- Pages/ViewProduct/ViewProduct.jsx
+++ Pages/ViewProduct/ViewProduct.jsx
@@ -1,15 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useProducts from '../../api/useProducts';
 
 const ViewProduct = () => {
-    const {productId } = useParams();
+    const { productId } = useParams();
     const [products] = useProducts();
+    const [showImage, setShowImage] = useState(null);
     const singleProduct = products.find(items => items?._id === productId);
-    console.log('This is product in single page', singleProduct)
+    const imageContainer = singleProduct?.images;
+    const firstImage = imageContainer[0]?.url;
+    useEffect(() => {
+        setShowImage(firstImage)
+    }, [productId])
+    console.log('This is product in single page', imageContainer)
     return (
         <div className='w-11/12 mx-auto'>
-            <h1>This is product page</h1>
+            <div className={`flex gap-2`}>
+                {/* image area */}
+                <div className='flex gap-2'>
+                    {
+                        imageContainer.length > 1 && (
+
+                            <div className='h-[422px] w-[140px] flex flex-col overflow-y-auto space-y-2.5'>
+                                {/* all images */}
+                                {
+                                    imageContainer.map((img, index) => (
+                                        <img src={img?.url} alt="" 
+                                        onClick={() => {setShowImage(img?.url)}}
+                                        className={`w-full h-[133px] object-cover rounded-md
+                                        ${showImage === img?.url ? 'border-2 border-blue-500 rounded-md' : ''}
+                                            `}
+                                        />
+                                    ))
+                                }
+
+                            </div>
+                        )
+                    }
+                    <div className='h-[422px]'>
+                    <img src={showImage} alt=""
+                    className='w-[400px] h-full object-cover rounded-md'
+                    />
+                    </div>
+                </div>
+                {/* text area */}
+                <div>
+                    <div className='flex flex-col space-y-2'>
+                        <h1 className='text-4xl font-bold uppercase'>{singleProduct?.title}</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
