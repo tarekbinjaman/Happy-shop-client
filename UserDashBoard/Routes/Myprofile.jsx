d566b24420ecf9e4b8ae67c9e44c480efe6e3048
tarekbinjaman <tarekbinjaman@gmail.com>
2025-10-04 01:06:44 +0600

    done

--- UserDashBoard/Routes/Myprofile.jsx
+++ UserDashBoard/Routes/Myprofile.jsx
@@ -9,10 +9,14 @@ const Myprofile = () => {
   const userData = currentUerData?.[0];
   return (
     <div className="flex justify-center items-center h-screen ">
-        <div className="flex flex-col items-center space-y-3 justify-between border px-2 py-6">
-      <div>
-        <img className="" src={userData?.photoURL} alt="" />
-      </div>
+        <div className="flex flex-col items-center space-y-3  border px-2 py-6 relative w-4xl">
+        <div className="bg-[#F2F0F1] h-40 w-full"></div>
+
+        <img className="rounded-full bg-[#F2F0F1] p-2 absolute top-22" src={userData?.photoURL} alt="" />
+
+
+
+
       <div>
         <h1 className="text-2xl">Name: <span>{userData?.name}</span></h1>
         <p className="text-sm text-center text-gray-500">{userData?.email}</p>
